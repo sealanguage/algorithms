b61c357c8ce6ec6fa472c2511fd57105b27a5e27
sealanguage <elaine.froehlich@gmail.com>
2019-11-14 19:21:38 -0500

    adding confirmTheEnding.js, finds a target number of letters at the end of a string and compares it with a target string, returns true or false if it is the same or different

--- confirmTheEnding.js
+++ confirmTheEnding.js
@@ -1,29 +1,40 @@
 function confirmEnding(str, target) {
   // "Never give up and good luck will find you."
   // -- Falcor
-  console.log("target ", target.length);
+  console.log("target length ", target.length);
+  console.log("target ", target);
   let str2;
   str2 = str.slice(str.length - target.length);
-  console.log("sliced ", str2);
+  console.log("str2 sliced ", str2);
 
-  var result = str.localeCompare(str2);
-  console.log("result ", result);
+  //   var result = str2.localeCompare(target);
+  //   console.log("result ", result);
+
+  if (str2 === target) {
+    console.log("true equal");
+    return true;
+  }
+  console.log("false not equal");
+  return false;
 
   //   if (str2 === str.slice(str.length - target.length));
   //   {
-  //     console.log("str2 and target", str2 + " " + target);
-  //     return true;
-  //   } else if {
-  //      return false;
+  //     console.log("str2 and target =", str2 + " + " + target);
+  //     return 1;
   //   }
-
-  return str;
+  return 0;
+  //   if (str2 != str.slice(str.length - target.length));
+  //   {
+  //     console.log("str2 not target =", str2 + " + " + target);
+  //     return false;
+  //   }
+  //   return str;
 }
 
 // need to return true and false
 
 confirmEnding("Open sesame", "same");
-
+confirmEnding("Open sesame", "pen");
 /*
 'abc'.length
 
