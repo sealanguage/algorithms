e074da8739fde290a83c5d1acd4e155ebdf841ea
sealanguage <elaine.froehlich@gmail.com>
2019-11-26 14:17:12 -0500

    adding exercises in algorithms, howMany.js and sumAllRestParamerers.js learning to use the rest parameter in ES6 and working on findElement.js to test an array against an argument and return the first case that tests true.

--- findElement.js
+++ findElement.js
@@ -1,9 +1,33 @@
 function findElement(arr, func) {
   let num = 0;
+  let arr1 = arr;
+  // num is passed to the function. We will need to set it to the elements we want to check with the function.
 
-  console.log("arr func is ", arr[func]);
+  console.log("arr is", arr);
+  for (var i = 0; i < arr.length; i++) {
+    console.log("arr1-i is ", arr1[i]); //  this gets the data of the array
+    num = arr1[i] % 2 === 0;
 
-  return num;
+    // if (num === true) {
+    //   return arr1[i] % 2;
+    // } else {
+    //   return undefined;
+    // }
+  }
+
+    console.log("num is", num);
+  }
+  return num % 2 === 0;
+
+  // return num;
 }
 
-findElement([1, 2, 3, 4], num => num % 2 === 0);
+findElement([1, 5, 2, 3, 4], num => num % 2 === 0);
+findElement([1, 3, 5, 8, 9, 10], function(num) {
+  return num % 2 === 0;
+}); //  should return 8.
+findElement([1, 3, 5, 9], function(num) {
+  return num % 2 === 0;
+}); //  should return undefined.
+
+// look through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
---
+++ suAllRestParameters.js
@@ -0,0 +1,10 @@
+function sumAll(...args) {
+   let sum = 0;
+   for (let arg of args) sum += arg;
+   return sum;
+
+}
+console.log(sumAll (1));
+console.log(sumAll (1, 2));
+console.log(sumAll (1,2,3));
+console.log(sumAll (1,2,3,4,5));
